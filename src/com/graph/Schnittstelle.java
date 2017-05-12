d8aa22181b199ddba6eb4ea69f77b6def69c62e7
li yong <ly05010419@gmail.com>
2017-05-12 14:34:20 +0200

    把测试文件分类放在文件夹中

--- src/com/graph/Schnittstelle.java
+++ src/com/graph/Schnittstelle.java
@@ -7,15 +7,20 @@ public class Schnittstelle {
 	public static void main(String[] args) throws Exception {
 
 		ArrayList<String> strList = new ArrayList<String>();
-//		 strList.add("./assets/G_1_2_1.txt");
-		strList.add("./assets/G_1_2.txt");
-		strList.add("./assets/G_1_20.txt");
-		strList.add("./assets/G_1_200.txt");
-		strList.add("./assets/G_10_20.txt");
-		strList.add("./assets/G_10_200.txt");
-//		strList.add("./assets/G_100_200.txt");
+		// strList.add("./assets/BreitensucheUndTiefensuche/Graph1.txt");
+		// strList.add("./assets/BreitensucheUndTiefensuche/Graph2.txt");
+		// strList.add("./assets/BreitensucheUndTiefensuche/Graph3.txt");
+		// strList.add("./assets/BreitensucheUndTiefensuche/Graph4.txt");
+		// strList.add("./assets/BreitensucheUndTiefensuche/Graph5.txt");
 
-		
+		// strList.add("./assets/MST/G_1_2_1.txt");
+		 strList.add("./assets/MST/G_1_2.txt");
+		 strList.add("./assets/MST/G_1_20.txt");
+		 strList.add("./assets/MST/G_1_200.txt");
+		 strList.add("./assets/MST/G_10_20.txt");
+		 strList.add("./assets/MST/G_10_200.txt");
+		// strList.add("./assets/MST/G_100_200.txt");
+
 		for (String str : strList) {
 
 			System.out.println(str.replace("./assets/", "").replace(".txt", ""));
@@ -25,10 +30,10 @@ public class Schnittstelle {
 			long startTime = System.currentTimeMillis();
 
 			// graph.breitenSuche();
-//			 graph.tiefenSuche();
-			
-//			 graph.prim();
-			graph.kruskal();
+			// graph.tiefenSuche();
+
+			// graph.prim();
+			 graph.kruskal();
 
 			long endTime = System.currentTimeMillis();
 			System.out.println("Alle Zeit：		" + (endTime - startTime) / (1000.0) + "s");
